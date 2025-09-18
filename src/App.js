ccbe5a9ab5e5d64e984f0234ad45d95c67f1e179
loisMac <155264617+loisMac@users.noreply.github.com>
2025-09-18 14:44:43 +0100

    Built form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from "./loomi.png";
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       <img src={logo} className="App-logo img-fluid"
       alt="logo"
       />
-      
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Lois Macleod</small>
+      </footer>
+      </div>
     </div>
   );
 }
